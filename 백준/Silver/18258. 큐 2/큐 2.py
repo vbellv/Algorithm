dc11d34834ed1ab2e3f4b7f3a0747b7e9b23aa5d
bell <120995065+vbellv@users.noreply.github.com>
2023-02-06 03:00:24 +0900

    [Silver IV] Title: 큐 2, Time: 1676 ms, Memory: 317772 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+commands = [input().strip() for _ in range(n)]
+queue = deque()
+
+for command in commands:
+    command = command.split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if queue:
+            pop = queue.popleft()
+            print(pop)
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        print(0) if queue else print(1)
+    elif command[0] == 'front':
+        print(queue[0]) if queue else print(-1)
+    elif command[0] == 'back':
+        print(-1) if not queue else print(queue[-1])
