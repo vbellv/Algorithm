7039febc5945425112b79da86582c7a72020dc61
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-06-18 22:28:36 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,4 +1,2 @@
 def solution(my_string):
-    num_list = [str(i) for i in range(10)]
-    answer = [int(num) for num in my_string if num in num_list]
-    return sorted(answer)
+    return sorted([int(num) for num in my_string if num.isdigit()])
