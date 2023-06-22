9523439e1c40e3635622dae63e753ca689718db3
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-06-22 12:01:37 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,4 +1,6 @@
-import string
+# def solution(age):
+#     import string
+#     return ''.join([string.ascii_lowercase[int(i)] for i in str(age)]) 
 
 def solution(age):
-    return ''.join([string.ascii_lowercase[int(i)] for i in str(age)])
+    return ''.join([chr(int(i)+97) for i in str(age)])
