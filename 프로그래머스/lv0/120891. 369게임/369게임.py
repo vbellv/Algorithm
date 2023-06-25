d8c0d0d79f35090df026841a4d15cfd2112646ae
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-06-25 09:45:40 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,6 +1,2 @@
 def solution(order):
-    answer = 0
-    for i in str(order):
-        if i in ['3', '6', '9']:
-            answer += 1
-    return answer
+    return sum(map(lambda x : str(order).count(str(x)), [3, 6, 9]))
