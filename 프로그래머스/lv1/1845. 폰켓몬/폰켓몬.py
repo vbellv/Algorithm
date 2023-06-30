c5c94cafe4a8a2d24af12da0fea88f14d12d7d66
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-07-01 00:51:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,2 @@
 def solution(nums):
-    if len(set(nums)) >= (len(nums)//2):
-        return len(nums)//2
-    elif len(set(nums)) < (len(nums)//2):
-        return len(set(nums))
-        
+    return min(len(set(nums)), len(nums)//2)
