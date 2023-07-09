e33730c6a2413b60d12771cafd35d2cba42ff1e0
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-07-09 19:14:17 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,18 +1,15 @@
+from collections import Counter
+
 def solution(array):
-    arr_dict = {}
-    check_list = []
     
-    for i in array:
-        arr_dict[i] = 0
-    
-    for i in array:
-        arr_dict[i] += 1
-    
-    for idx, val in arr_dict.items():
-        if val == max(arr_dict.values()):
-            check_list.append(idx)
-            
-    if len(check_list) >= 2:
+    count = Counter(array)
+    cnt = 0
+
+    for idx, val in count.items():
+        if val == max(count.values()):
+            cnt += 1
+        
+    if cnt >= 2:
         return -1
-    else:
-        return check_list[0]
+    elif cnt == 1:
+        return count.most_common(1)[0][0]
