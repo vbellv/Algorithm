80c6128e117419320491990f3e7ad8a7d7d0be1d
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-07-19 18:12:26 +0900

    [level 1] Title: 최소직사각형, Time: 3.69 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,4 @@
 def solution(sizes):
     sizes = list(sorted(i) for i in sizes)
     
-    left_num = [i[0] for i in sizes]
-    right_num = [i[1] for i in sizes]
-    
-    return max(left_num) * max(right_num)
+    return max([i[0] for i in sizes]) * max([i[1] for i in sizes])
