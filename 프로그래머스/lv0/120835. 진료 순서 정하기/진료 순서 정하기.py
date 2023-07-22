77502e8d9b292bba9c16ac3de5a78616fdfd9554
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-07-22 18:17:57 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,13 +1,12 @@
 def solution(emergency):
-    check_idx, check_val, answer = [], [], []
+    check_list, answer = [], []
     
     for idx, val in enumerate(sorted(emergency, reverse=True)):
-        check_idx.append(idx+1)
-        check_val.append(val)
+        check_list.append((idx+1, val))
         
-    for i in range(len(check_idx)):
-        for j in range(len(check_idx)):
-            if emergency[i] == check_val[j]:
-                answer.append(check_idx[j])
+    for i in range(len(check_list)):
+        for j in range(len(check_list)):
+            if emergency[i] == check_list[j][1]:
+                answer.append(check_list[j][0])
     
     return answer
