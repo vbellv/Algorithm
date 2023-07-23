99db4f22085476d31817abbd09d3ab22f1c66b78
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-07-23 23:25:15 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,10 +1,10 @@
 def solution(array, n):
+    array.sort()
     check_list = []
     
     for i in array:
         check_list.append((i, abs(i-n)))
-        
-    check_list = sorted(check_list, key=lambda x : x[0])
+
     check_list = sorted(check_list, key=lambda x : x[1])
     
     return check_list[0][0]
