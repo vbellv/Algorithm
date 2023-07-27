66d70bc6229c4ae6680a1aacf54e1585969f9b91
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-07-27 23:26:48 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(A, B):
+    A = deque([i for i in A])
+    B = deque([i for i in B])
+    cnt = 0
+    
+    if A == B:
+        return cnt
+    elif A != B:
+        while True:
+            A.rotate()
+            cnt += 1
+            if A == B:
+                return cnt
+            elif cnt > len(A):
+                return -1
+    
