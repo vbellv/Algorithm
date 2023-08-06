fb4c3182a898e79f38e47e7231f7f1639d214955
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-08-06 16:26:30 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,10 +1,2 @@
 def solution(my_string):
-    my_string = my_string.split()
-    
-    for i in range(len(my_string)):
-        if my_string[i] == '+':
-            my_string[i+1] = int(my_string[i-1]) + int(my_string[i+1])
-        elif my_string[i] == '-':
-            my_string[i+1] = int(my_string[i-1]) - int(my_string[i+1])
-    
-    return my_string[-1]
+    return eval(my_string)
