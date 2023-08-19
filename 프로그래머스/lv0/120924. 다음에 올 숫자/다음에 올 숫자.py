c8746b5661c00aae41e0f4dd2898e2be111cc216
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-08-19 12:41:24 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(common):
+    if common[1] - common[0] == common[2] - common[1]:
+        return common[-1] + (common[1] - common[0])
+    else:
+        return common[-1] * (common[1] // common[0])
+        
+    
+print(solution([-1, -2, -3, -4]))
+print(solution([-1, 1, -1, 1]))
+print(solution([-1, 2, -4, 8]))
