248484c71cd1365ed6e7cedddd8d930715964e5b
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-08-24 20:34:10 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,12 @@
 def solution(numbers):
+    answer = ''
+    
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x*3 ,reverse=True)
     
-    return str(int(''.join(numbers)))
+    for i in numbers:
+        answer += i
+    
+    return str(int(answer))
 
 print(solution([0, 0, 0]))
