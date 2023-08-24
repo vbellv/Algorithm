3c80a3229813127eb11447aba7cbd516777c391f
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-08-24 21:32:23 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,17 @@
-def solution(numbers):
-    answer = ''
-    
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3 ,reverse=True)
-    
-    for i in numbers:
-        answer += i
-    
-    return str(int(answer))
+from functools import cmp_to_key
 
-print(solution([0, 0, 0]))
+def compare(num1, num2):
+    if num1 + num2 > num2 + num1:
+        return -1
+    elif num1 + num2 == num2 + num1:
+        return 0
+    else:
+        return 1
+
+    
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers = sorted(numbers, key=cmp_to_key(compare))
+    return str(int(''.join(numbers)))
+
+print(solution([0, 0, 0]))   
