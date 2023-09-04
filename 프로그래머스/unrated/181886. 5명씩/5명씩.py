5660e89792dfb6e9969ac3b98b6e3dbe3801c119
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-04 23:02:11 +0900

    [unrated] Title: 5명씩, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.py
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -1,8 +1,4 @@
 import math
 
 def solution(names):
-    answer = []
-    
-    for i in range(math.ceil(len(names) / 5)):
-        answer.append(names[i*5])
-    return answer
+    return [names[i*5] for i in range(math.ceil(len(names) / 5))]
