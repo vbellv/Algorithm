acc9aff35d7d23b4cbae0ce0b8520ccaebe0937a
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-06 02:03:49 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -6,8 +6,10 @@ def solution(keyinput, board):
     HEIGHT = (board[1]-1) // 2
     
     for key in keyinput:
+        # 각 좌표값을 더했을 때 범위를 넘어가면 넘기기
         if start_point[0] + key_dict[key][0] > WIDTH or start_point[0] + key_dict[key][0] < -WIDTH or start_point[1] + key_dict[key][1] > HEIGHT or start_point[1] + key_dict[key][1] < -HEIGHT:
             continue
+        # 그렇지 않은 경우 각 좌표값을 더해주기
         else:
             start_point[0] += key_dict[key][0]
             start_point[1] += key_dict[key][1]
