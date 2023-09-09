bd802a0170c9da9230b3cc3479fe175995a2b3f4
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-09 12:46:42 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -1,24 +1,3 @@
 def solution(score):
-    ranks = sorted([sum(scores) for scores in score], reverse=True)
-    
-    rank_score, cnt, check = 201, 0, 0
-    rank_dict = {}
-    
-    for rank in ranks:
-        if rank < rank_score:
-            cnt += 1
-
-            if check != 0:
-                cnt += check
-                check = 0
-
-            rank_dict[rank] = cnt
-            rank_score = rank
-
-        elif rank == rank_score:
-            rank_dict[rank] = cnt
-            check += 1   
-
-    answer = [rank_dict[sum(scores)] for scores in score]
-    
-    return answer
+    rank_list = sorted([sum(scores) for scores in score], reverse = True)
+    return [rank_list.index(sum(scores))+1 for scores in score]
