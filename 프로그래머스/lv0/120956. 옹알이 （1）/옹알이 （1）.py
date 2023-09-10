740c48c8aa4c9c1c1979910660a6f062273ab64d
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-10 18:56:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+def solution(babbling):
+    baby = ["aya", "ye", "woo", "ma"]
+    word_list = []
+    cnt = 0
+    
+    words = list(permutations(baby, 2)) + list(permutations(baby, 3)) + list(permutations(baby, 4))
+    
+    
+    for word in words:
+        word_list.append(''.join(word))
+    word_list += baby
+    
+    for word in babbling:
+        if word in word_list:
+            cnt += 1
+            
+    return cnt
