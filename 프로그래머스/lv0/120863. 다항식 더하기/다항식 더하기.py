707eb1a9c45ba93f1dd0f75357bf05e79a2212d3
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-18 18:39:50 +0900

    [level 0] Title: 다항식 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,43 @@
+def solution(polynomial):
+    x_list, num_list = [], []
+    
+    polynomial = polynomial.split(' + ')
+    
+    for i in polynomial:
+        if 'x' in i:
+            x_list.append(i)
+        else:
+            num_list.append(i)
+            
+    
+    for i in range(len(x_list)):
+        if 'x' == x_list[i]:
+            x_list[i] = x_list[i].replace('x', '1')
+        else:
+            x_list[i] = x_list[i].replace('x', '')
+            
+    x_sum = sum(list(map(int, x_list)))
+    num_sum = sum(list(map(int, num_list)))
+    
+    if num_sum > 0:
+        if x_sum == 1:
+            return 'x + ' + str(num_sum)
+        elif x_sum == 0:
+            return str(num_sum)
+        else:
+            return str(x_sum) + 'x + ' + str(num_sum) 
+
+    # num_sum == 0
+    else:  
+        if x_sum != 1:
+            return str(x_sum) + 'x'
+        elif x_sum == 0:
+            return 0
+        else:
+            return 'x'
+        
+            
+print(solution('x'))
+print(solution('1'))
+print(solution('3x + x'))
+print(solution('x + 1'))
