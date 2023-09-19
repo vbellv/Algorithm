7f1cbddd7c19006220d2576783efce66f2c52f56
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-19 20:11:47 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,16 @@
+def solution(n):
+    except_three_list = []
+    num = 0
+    
+    while True:
+        if num % 3 != 0:
+            if '3' not in str(num):
+                except_three_list.append(num)
+        
+        num += 1
+        
+        if len(except_three_list) == 100:
+            break
+        
+    return except_three_list[n-1]
+    
