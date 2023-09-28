beed4c057eb3fdcf230d8f36d1608a850b84ca70
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-09-28 22:28:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,16 +1,2 @@
-def harshad_num(x):
-    num = 0
-    
-    while True:
-        if len(str(x)) == 1:
-            num += x
-            break
-            
-        num += x % 10
-        x //= 10
-        
-    return num
-
 def solution(x):
-    num = harshad_num(x)
-    return True if x % num == 0 else False
+    return x % sum(int(num) for num in str(x)) == 0
