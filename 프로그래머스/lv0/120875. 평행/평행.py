e9be55e2ec1c68fb22792e60e28af97e06671f98
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-10-05 22:48:18 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,16 +1,20 @@
-def solution(dots):
+def slope_line(dots):
     check_list = []
-    cnt = 0
     
     for i in range(len(dots)):
         for j in range(i+1, len(dots)):
             check_list.append(abs(dots[i][1]-dots[j][1]) / abs(dots[i][0]-dots[j][0]))
-                
+            
+    return check_list
+
+def solution(dots):
+    check_list = slope_line(dots)
+    cnt = 0
+           
     for idx in range(len(check_list)//2):
         if check_list[idx] == check_list[-(idx+1)]:
             cnt += 1
-            
-            if cnt != 0: break
+            break
     
     if cnt != 0: return 1
     else: return 0
