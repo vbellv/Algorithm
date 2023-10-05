736618b0977cf65a9d21a824b1759f8f9f61c014
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-10-05 22:43:25 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -0,0 +1,19 @@
+def solution(dots):
+    check_list = []
+    cnt = 0
+    
+    for i in range(len(dots)):
+        for j in range(i+1, len(dots)):
+            check_list.append(abs(dots[i][1]-dots[j][1]) / abs(dots[i][0]-dots[j][0]))
+                
+    for idx in range(len(check_list)//2):
+        if check_list[idx] == check_list[-(idx+1)]:
+            cnt += 1
+            
+            if cnt != 0: break
+    
+    if cnt != 0: return 1
+    else: return 0
+
+print(solution([[1, 2], [5, 1], [3, 6], [6, 3]])) # 1
+print(solution([[3, 5], [4, 4], [8, 9], [6, 11]])) # 1
