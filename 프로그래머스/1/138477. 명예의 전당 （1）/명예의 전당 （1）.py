d40e15447d6800e900fa264a220a91bf0be0d9ef
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-10-18 22:57:08 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,12 @@
+def solution(k, score):
+    score_list, answer = [], []
+    
+    for idx in range(len(score)):
+        score_list.append(score[idx])
+        score_list.sort(reverse=True)
+
+        answer.append(score_list[:k][-1])
+            
+    return answer
+            
+            
