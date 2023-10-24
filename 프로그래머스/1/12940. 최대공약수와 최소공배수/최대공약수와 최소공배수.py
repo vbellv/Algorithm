ed2d19dbebca9a139abb87bb989aba8e77553434
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-10-24 20:38:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,10 @@
+def solution(n, m):
+    lcm, gcd = 0, 0
+    
+    for num in range(1, (max(n, m)//2+1)):
+        if n % num == 0 and m % num == 0:
+            lcm = num
+            
+    gcd = (n*m) // lcm
+    
+    return [lcm, gcd]
