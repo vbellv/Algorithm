d0e6460e3b57099a4a80f8d6bb6af1ca89e2f305
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-10-27 01:45:40 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,6 +1,2 @@
 def solution(s):
-    alphabet_list = list(map(chr, range(122, 96, -1))) + [word.upper() for word in list(map(chr, range(122, 96, -1)))]
-    
-    check_list = sorted([(alphabet_list.index(word), word) for word in s], key=lambda x: x[0])
-            
-    return ''.join([word[1] for word in check_list])
+    return ''.join(sorted(s, reverse=True))
