b37cf25f83c8a61ac9c73b3be33b2bfe436c905c
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-10-30 01:13:33 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,23 +1,13 @@
-def user_dictionary(id_list, report):
-    report_dict, user_dict = {}, {}
-    
-    for id in id_list:
-        report_dict[id] = 0
-        user_dict[id] = 0
-    
-    for names in set(report):
-        report_dict[names.split(' ')[1]] += 1
-        
-    return report_dict, user_dict
+from collections import Counter
 
 def solution(id_list, report, k):
-    report_dict, user_dict = user_dictionary(id_list, report)
+    reports = Counter([names.split(' ')[1] for names in set(report)])
+    suspended_users = [name for name, cnt in reports.items() if cnt >= k]
     
-    report_user = [key for key, val in report_dict.items() if val >= k]
-    cnt = 0
+    send_emails = dict.fromkeys(id_list, 0)
     
     for names in set(report):
-        if names.split(' ')[1] in report_user:
-            user_dict[names.split(' ')[0]] += 1
+        if names.split(' ')[1] in suspended_users:
+            send_emails[names.split(' ')[0]] += 1
             
-    return list(user_dict.values())
+    return list(send_emails.values())
