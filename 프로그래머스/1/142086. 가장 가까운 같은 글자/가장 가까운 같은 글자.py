160e746c6a99bf8f9ca42fdcf0e69abdc40681dd
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-11-05 22:42:08 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.45 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,24 +1,26 @@
 def solution(s):
-    word_all_index, word_index_list = [], []
+    print(list(s))
+    print(list(filter(lambda x: s[x] == 'a', list(s))))
+#     word_all_index, word_index_list = [], []
     
-    # 중복되는 단어에 대한 모든 인덱스 저장
-    for word in set(s):
-        word_all_index.append((word, list(filter(lambda x: s[x] == word, range(len(s))))))
+#     # 중복되는 단어에 대한 모든 인덱스 저장
+#     for word in set(s):
+#         word_all_index.append((word, list(filter(lambda x: s[x] == word, list(s)))))
     
-    # index의 길이가 1이면 -1 저장, 1을 초과하면 index를 순회하며 1의 위치만큼 차이값을 저장
-    for word, index in word_all_index:
-        if len(index) == 1:
-            for num in range(len(index)):
-                word_index_list.append((word, -1, index[num]))
-        else:
-            for num in range(len(index)):
-                if num == 0:
-                    word_index_list.append((word, -1, index[num]))
-                elif num > 0:
-                    word_index_list.append((word, index[num] - index[num-1], index[num]))
+#     # index의 길이가 1이면 -1 저장, 1을 초과하면 index를 순회하며 1의 위치만큼 차이값을 저장
+#     for word, index in word_all_index:
+#         if len(index) == 1:
+#             for num in range(len(index)):
+#                 word_index_list.append((word, -1, index[num]))
+#         else:
+#             for num in range(len(index)):
+#                 if num == 0:
+#                     word_index_list.append((word, -1, index[num]))
+#                 elif num > 0:
+#                     word_index_list.append((word, index[num] - index[num-1], index[num]))
     
-    # 원래 단어에 해당하는 인덱스 순서대로 정렬
-    word_index_list = sorted(word_index_list, key=lambda x: x[2])
+#     # 원래 단어에 해당하는 인덱스 순서대로 정렬
+#     word_index_list = sorted(word_index_list, key=lambda x: x[2])
     
-    # 구하려는 글자 위치값 반환
-    return [num for _, num, _ in word_index_list]
+#     # 구하려는 글자 위치값 반환
+#     return [num for _, num, _ in word_index_list]
