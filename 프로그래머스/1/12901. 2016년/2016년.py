f26b05af375bba559bc867b41bc3deb90eb80a8e
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-11-10 01:07:48 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -2,10 +2,6 @@ def solution(a, b):
     calendar_dict = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     day_list = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
     
-    days = 0
-    
-    for num in range(1, a):
-        days += calendar_dict[num]
-    days = (days + b) % 7 - 1
+    days = (sum(calendar_dict[num] for num in range(1, a)) + b) % 7 - 1
     
     return day_list[days]
