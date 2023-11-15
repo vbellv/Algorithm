5fa69dca8f7aae0df8a2d3a037fda338fd0c5c31
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-11-16 01:03:24 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.79 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,18 +1,46 @@
+from collections import deque
+
+def new_board(board):
+    total_board = []
+    
+    for i in range(len(board)):
+        temp_list = []
+        for j in range(len(board)):
+            if board[j][i] != 0:
+                temp_list.append(board[j][i])
+        total_board.append(temp_list)
+    
+    return total_board
+
 def solution(board, moves):
-    stack = []
+    moves = deque(moves)
+    board = new_board(board)
+    answer = []
     cnt = 0
     
-    for move in moves:
-        for idx in range(len(board)):
-            if board[idx][move-1] != 0:
-                stack.append(board[idx][move-1])
-                board[idx][move-1] = 0
+    while len(moves) > 0:
+        move = moves.popleft() - 1
+        
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                try:
+                    if move == i:
+                        board[i] = deque(board[i])
+                        print(board[i].popleft())
+                        pop_num = board[i].popleft()
+                        answer.append(pop_num)
+                        cnt += 1
+                except:
+                    pass
                 
-                if len(stack) >= 2:
-                    if stack[-2] == stack[-1]:
-                        stack.pop()
-                        stack.pop()
-                        cnt += 2      
-                break
-            
-    return cnt
+                try:
+                    check_num = answer.pop()
+                    if check_num == pop_num:
+                        pass
+                    else:
+                        answer.append(check_num)
+                        answer.append(pop_num) 
+                except:
+                    pass
+    
+    return cnt - len(answer)
