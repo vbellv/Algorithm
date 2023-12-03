6d0412ec0556be8a3c8569a820ccd51819cbd5a1
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-12-04 00:53:04 +0900

    [level unrated] Title: x 사이의 개수, Time: 1.61 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,13 +1,2 @@
 def solution(myString):
-    answer = []
-    num = 0
-    
-    for word in myString:
-        if word != 'x':
-            num += 1
-        elif word == 'x':
-            answer.append(num)
-            num = 0
-    answer.append(num)
-    
-    return answer
+    return [len(word) for word in myString.split('x')]
