5c49cb97c9ffe1b52c6f81796e7a7b1396e4b0ff
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-12-18 19:17:52 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,15 +1,7 @@
 def solution(date1, date2):
-    if date1[0] > date2[0]:
-        return 0
-    elif date1[0] < date2[0]:
-        return 1
-    else:
-        if date1[1] > date2[1]:
+    for idx in range(3):
+        if date1[idx] > date2[idx]:
             return 0
-        elif date1[1] < date2[1]:
+        elif date1[idx] < date2[idx]:
             return 1
-        else:
-            if date1[2] >= date2[2]:
-                return 0
-            else:
-                return 1
+    return 0
