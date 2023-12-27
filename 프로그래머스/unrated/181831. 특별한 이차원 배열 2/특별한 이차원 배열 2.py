3777c37a2db3ccaf9d486d51a1d437624be21d3b
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2023-12-27 21:56:00 +0900

    [level unrated] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,12 +1,6 @@
 def solution(arr):
-    answer = []
-    
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            if arr[i][j] == arr[j][i]:
-                answer.append(0)
-            else:
-                answer.append(1)
-                
-    if sum(answer) == 0: return 1
-    else: return 0
+            if arr[i][j] != arr[j][i]:
+                return 0
+    return 1
