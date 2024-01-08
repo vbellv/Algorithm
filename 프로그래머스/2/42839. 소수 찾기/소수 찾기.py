c4dae3dda14332241705daa64e40ef3904e63bb6
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-01-08 17:27:24 +0900

    [level 2] Title: 소수 찾기, Time: 1.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -2,15 +2,14 @@ from itertools import permutations
 
 def solution(numbers):
     numbers = list(numbers)
-    total_list = []
+    total_list = list()
     
     for i in range(1, len(numbers)+1):
         com = list(permutations(numbers, i))
         for nums in com:
             total_list.append(''.join(nums))
     
-    total_list = list(set(list(map(int, total_list))))
-    total_list = list(num for num in total_list if num > 1)
+    total_list = list(num for num in set(list(map(int, total_list))) if num > 1)
     
     not_in_list = []
     
