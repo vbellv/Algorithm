fb5c568aaeacaa1acf6d742b2fd25a4f4bee5c62
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-01-24 21:28:35 +0900

    [level 2] Title: 짝지어 제거하기, Time: 85.18 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    stack = []
+    
+    for word in s:
+        if not stack:
+            stack.append(word)
+        else:
+            if stack[-1] == word:
+                stack.pop()
+            else:
+                stack.append(word)
+    
+    if not stack:
+        return 1
+    else:
+        return 0
+        
