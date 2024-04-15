6dabe86716008f4b00ce3f8267ead8351f57692a
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-04-16 08:59:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,12 @@
+def solution(n):
+    binary_num = bin(n)[2:]
+    count_one = str(binary_num).count('1')
+    
+    while True:
+        n += 1
+        binary_num = bin(n)[2:]
+        new_count_one = str(binary_num).count('1')
+        new_count_zero = str(binary_num).count('0')
+
+        if new_count_one == count_one:
+            return n
