45c70951062c50a035b874fb145e302da845948d
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-04-18 10:43:43 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,31 @@
+def solution(keymap, targets):
+    target_dict = {}
+    answer = []
+    
+    total_targets = keymap + targets
+    
+    for target in total_targets:
+        for word in target:
+            target_dict[word] = 999999
+    
+    for key in keymap:
+        for idx in range(len(key)):
+            try:
+                if target_dict[key[idx]] > idx + 1:
+                    target_dict[key[idx]] = idx + 1
+            except:
+                pass
+    
+    for target in targets:
+        cnt = 0
+        for word in target:
+            cnt += target_dict[word]
+        answer.append(cnt)
+        
+    for idx in range(len(answer)):
+        if answer[idx] >= 999999:
+            answer[idx] = -1
+
+    return answer
+
+    
