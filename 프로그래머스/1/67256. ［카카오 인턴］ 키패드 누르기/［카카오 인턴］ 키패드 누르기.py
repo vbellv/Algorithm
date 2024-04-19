6216b2692d63ece37ae03d77f7d2723a9df45bbe
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-04-19 18:37:25 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -23,33 +23,24 @@ def solution(numbers, hand):
             answer += 'R'
             right_hand = number
         else:
-            # print('num', number, 'left', left_hand, 'right', right_hand)
             left_distance, right_distance = 0, 0
             
             if left_hand in [1, 4, 7, 10] and right_hand in [3, 6, 9, 11]:
                 left_distance = calculate_distance(numbers_dict, left_hand, number, edge_keypad_distance)
                 right_distance = calculate_distance(numbers_dict, right_hand, number, edge_keypad_distance)
-                
-                # print('1', left_distance, right_distance)
-                
+                                
             elif left_hand in [2, 5, 8, 0] and right_hand in [3, 6, 9, 11]:
                 left_distance = calculate_distance(numbers_dict, left_hand, number, median_keypad_distance)
                 right_distance = calculate_distance(numbers_dict, right_hand, number, edge_keypad_distance)
-                
-                # print('2', left_distance, right_distance)
-                
+                                
             elif left_hand in [1, 4, 7, 10] and right_hand in [2, 5, 8, 0]:
                 left_distance = calculate_distance(numbers_dict, left_hand, number, edge_keypad_distance)
                 right_distance = calculate_distance(numbers_dict, right_hand, number, median_keypad_distance)
-                
-                # print('3', left_distance, right_distance)
-                
+                                
             elif left_hand in [2, 5, 8, 0] and right_hand in [2, 5, 8, 0]:
                 left_distance = calculate_distance(numbers_dict, left_hand, number, median_keypad_distance)
                 right_distance = calculate_distance(numbers_dict, right_hand, number, median_keypad_distance)
-                
-                # print('4', left_distance, right_distance)
-            
+                            
             if left_distance > right_distance:
                 answer += 'R'
                 right_hand = number
@@ -64,10 +55,7 @@ def solution(numbers, hand):
                     if hand == 'left':
                         answer += 'L'
                         left_hand = number
-                        
-            # print('ans', answer)
-            # print('==============')
-            # print()
+
     return answer
             
             
