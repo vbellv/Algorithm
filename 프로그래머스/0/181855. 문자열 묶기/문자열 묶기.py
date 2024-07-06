dd662a4c59f0c9686b1f8e084655ecb380f999d6
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-07-06 23:37:58 +0900

    [level 0] Title: 문자열 묶기, Time: 8.49 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,14 +1,7 @@
 def solution(strArr):
-    count_of_word = dict.fromkeys(list(set([len(word) for word in strArr])), 0)
-    count = float('-inf')
-    answer = 0
-
-    for word in strArr:
-        count_of_word[len(word)] += 1
+    length = [0] * 31
     
-    for key, value in count_of_word.items():
-        if value >= count:
-            count = value
-            answer = value
-            
-    return answer
+    for word in strArr:
+        length[len(word)] += 1
+        
+    return max(length)
