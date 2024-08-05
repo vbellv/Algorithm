f8f2e39a66f5efc45611fbb816f24527aaaafeff
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-08-05 23:39:26 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,9 +1,3 @@
 def solution(rank, attendance):
-    ranks = []
-    
-    for i in range(len(rank)):
-        if attendance[i]:
-            ranks.append((rank[i], i))
-            
-    ranks = sorted(ranks, key=lambda x: x[0])
+    ranks = sorted([(rank[i], i) for i in range(len(rank)) if attendance[i]], key=lambda x: x[0])
     return 10000 * ranks[0][1] + 100 * ranks[1][1] + ranks[2][1]
