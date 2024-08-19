dc09a26ca270ca7d39c9a7b5f6ce4f13f4a6bc1b
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-08-19 09:58:23 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -2,18 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-commands = [input().strip() for _ in range(n)]
 stack = []
 
-for command in commands:
-    command = command.split()
+for _ in range(n):
+    command = list(map(str, input().split()))
+
     if command[0] == 'push':
         stack.append(command[1])
-    elif command[0] == 'pop':
-        print(-1) if not stack else print(stack.pop())
-    elif command[0] == 'size':
+    if command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    if command[0] == 'size':
         print(len(stack))
-    elif command[0] == 'empty':
-        print(0) if stack else print(1)
-    elif command[0] == 'top':
-        print(stack[-1]) if stack else print(-1)
+    if command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    if command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
