2eb316694740576d5f3fa6d8ad443fbadf1e733c
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-08-28 23:42:29 +0900

    [level 0] Title: 배열 만들기 6, Time: 185.31 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -2,14 +2,9 @@ def solution(arr):
     stack = []
     
     for i in range(len(arr)):
-        if not stack:
-            stack.append(arr[i])
-        elif stack and stack[-1] == arr[i]:
+        if stack and stack[-1] == arr[i]:
             stack.pop()
-        elif stack and stack[-1] != arr[i]:
+        else:
             stack.append(arr[i])
     
-    if not stack:
-        return [-1]
-    else:
-        return stack
+    return stack if stack else [-1]
