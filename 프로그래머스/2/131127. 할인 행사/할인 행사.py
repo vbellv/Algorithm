1bf925588421539d392b297e09d9f543f8dd7e3b
Jongeun Lee <120995065+vbellv@users.noreply.github.com>
2024-09-02 11:36:48 +0900

    [level 2] Title: 할인 행사, Time: 186.53 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -4,9 +4,7 @@ def make_product_dict(want, number):
     wants = dict()
     for product, count in zip(want, number):
         wants[product] = count
-    
-    wants = dict(sorted(wants.items(), key=lambda x: (x[1], x[0]), reverse=True))
-    
+        
     return wants
 
 
@@ -16,7 +14,6 @@ def solution(want, number, discount):
     
     for index in range(len(discount)):
         ten_days = dict(Counter(discount[index:index + 10]))
-        ten_days = dict(sorted(ten_days.items(), key=lambda x: (x[1], x[0]), reverse=True))
         
         if wants == ten_days:
             answer += 1
